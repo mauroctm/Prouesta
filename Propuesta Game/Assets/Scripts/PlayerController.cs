8bed5ed64c71a69c670a8e1f9f9fc3f4309e4733
mauroctm <mauroctm7@gmail.com>
2017-02-13 21:37:04 -0500

    Add new Player

---
+++ Propuesta Game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed;
+	private Rigidbody rb;
+	private Rigidbody rbpickUp;
+	private float x = -6f;
+	private float y = 35f;
+	private float z = -23f;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+		rb.useGravity = true;
+	}
+
+	void FixedUpdate ()
+	{
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+
+		Vector3 movement = new Vector3 (moveHorizontal, 0, moveVertical);
+
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		print ("Entro");
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+			//other.gameObject.transform.position = new Vector3(x,y,z);
+			other.gameObject.transform.localPosition  = new Vector3(x,y,z);
+			rbpickUp = other.gameObject.GetComponent<Rigidbody> ();
+			rbpickUp.velocity = Vector3.zero;
+			other.gameObject.SetActive (true);
+		}
+	}
+}
---
+++ Propuesta Game/Assets/Scripts/Rotate.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+	public float angularVelocity = 10.0f;
+	private Vector3 axisOfRotation;
+	// Use this for initialization
+	void Start () {
+		axisOfRotation = Random.onUnitSphere;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate(axisOfRotation, angularVelocity * Time.smoothDeltaTime);
+
+	}
+}
